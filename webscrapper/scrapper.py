9239f03445f895fa35751c9ae602191db168a5df
bellDev-code <sumaoo20@naver.com>
2023-08-08 13:28:56 +0900

    scrapper Error fix

--- webscrapper/scrapper.py
+++ webscrapper/scrapper.py
@@ -1,20 +1,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import subprocess
-import shutil
-
-try:
-    shutil.rmtree(r"c:\chrometemp")
-except FileNotFoundError:
-    pass
-
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-browser = webdriver.Chrome(options=chrome_options)
-
 
 def get_page_count(keyword):
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    browser = webdriver.Chrome(options=chrome_options)
     base_url = 'https://kr.indeed.com/jobs?q='
     browser.get(f"{base_url}{keyword}")
 
@@ -23,36 +14,45 @@ def get_page_count(keyword):
     
     if pagination == None:
         return 1
-    pages = pagination.find('div', recursive=False)
-    print(len(pages))
+    pages = pagination.find_all('div', recursive=False)
+    count = len(pages)
     
-get_page_count("python")
+    if count >= 5:
+        return 5
+    else:
+        return count
 
 def extract_indeed_jobs(keyword):
-    base_url = 'https://kr.indeed.com/jobs?q='
-    browser.get(f"{base_url}{keyword}")
-
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    browser = webdriver.Chrome(options=chrome_options)
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        base_url = 'https://kr.indeed.com/jobs?q='
+        browser.get(f"{base_url}{keyword}")
     results = []
     soup = BeautifulSoup(browser.page_source, "html.parser")
     job_list = soup.find("ul", class_='jobsearch-ResultsList')
-    jobs = job_list.find_all('li', recursive=False)
-    for job in jobs:
-        zone = job.find("div", class_="mosaic-zone")
-        if zone == None:
-            anchor = job.select_one("h2 a")
-            title = anchor['aria-label']
-            link = anchor['href']
-            name = job.find("span", class_= "companyName")
-            location = job.find("div", class_="companyLocation")
-            job_data = {
-                'link': f"https://kr.indeed.com{link}",
-                'company': name.string,
-                'location': location.string,
-                'position': title,
-            }
-            results.append(job_data)      
+    if job_list is not None:
+        jobs = job_list.find_all('li', recursive=False) 
+        for job in jobs:
+            zone = job.find("div", class_="mosaic-zone")
+            if zone == None:
+                anchor = job.select_one("h2 a")
+                title = anchor['aria-label']
+                link = anchor['href']
+                name = job.find("span", class_= "companyName")
+                location = job.find("div", class_="companyLocation")
+                job_data = {
+                    'link': f"https://kr.indeed.com{link}",
+                    'company': name.string,
+                    'location': location.string,
+                    'position': title,
+                }
+                results.append(job_data)
     for result in results:
         print(result, "/////\n///////")
+    
         
         
-extract_indeed_jobs("python")
+extract_indeed_jobs("ruby")
